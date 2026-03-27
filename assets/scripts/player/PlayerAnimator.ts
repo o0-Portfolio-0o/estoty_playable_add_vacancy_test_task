ab1e51e12b5a110762f1f680b731035a69fc9f39
o0pmitev <pmitev.dev@gmail.com>
2026-03-27 19:28:33 +0200

    feat: add player animations idle and run

---
+++ assets/scripts/player/PlayerAnimator.ts
@@ -0,0 +1,38 @@
+import { _decorator, Component, SkeletalAnimation} from 'cc';
+const { ccclass, property } = _decorator;
+
+export enum AnimationState {
+    IDLE = 'Armature.001|Armature.001|IDLE',
+    RUN = 'Armature.001|Armature.001|RUN',
+    AXE = 'Armature.001|Armature.001|AXE',
+};
+
+
+@ccclass('PlayerAnimator')
+export class PlayerAnimator extends Component {
+
+    @property(SkeletalAnimation)
+    public animation: SkeletalAnimation = null;
+
+    private _currentState: AnimationState = AnimationState.IDLE;
+
+    onLoad(): void {
+        if (!this.animation) {
+            this.animation = this.getComponent(SkeletalAnimation);
+        }
+        this.playAnimation(AnimationState.IDLE);
+    }
+
+    public playAnimation(state: AnimationState): void {
+        if (this._currentState === state) return;
+
+        this._currentState = state;
+        this.animation.play(state);
+    }
+
+    public isPlaying(state: AnimationState): boolean {
+        return this._currentState === state;
+    }
+}
+
+
--- assets/scripts/player/PlayerController.ts
+++ assets/scripts/player/PlayerController.ts
@@ -1,4 +1,6 @@
 import { _decorator, Camera, Component, EventTouch, geometry, Input, input, Vec3 } from 'cc';
+import { PlayerAnimator, AnimationState } from './PlayerAnimator';
+
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
@@ -8,6 +10,9 @@ export class PlayerController extends Component {
 
     @property(Camera)
     public camera: Camera = null;
+
+    @property(PlayerAnimator)
+    public animator: PlayerAnimator = null;
 
     private targetPosition: Vec3 = new Vec3();
     private isMoving: boolean = false;
@@ -24,6 +29,45 @@ export class PlayerController extends Component {
         input.off(Input.EventType.TOUCH_START, this._onTouchStart, this);
         input.off(Input.EventType.TOUCH_MOVE, this._onTouchMove, this);
         input.off(Input.EventType.TOUCH_END, this._onTouchEnd, this);
+    }
+
+    update(deltaTime: number) {
+        if (!this.isMoving) {
+            this.animator?.playAnimation(AnimationState.IDLE);
+            console.log(this.animator.isPlaying(AnimationState.IDLE))
+            return;
+        };
+            console.log(this.animator.isPlaying(AnimationState.IDLE))
+        const currentPosition = this.node.worldPosition;
+        const direction = new Vec3();
+        Vec3.subtract(direction, this.targetPosition, currentPosition);
+        direction.y = 0;
+
+        const distance = direction.length();
+
+        if (distance < this.STOP_DISTANCE) {
+            this.isMoving = false;
+            this.animator?.playAnimation(AnimationState.IDLE)
+            return;
+        }
+
+        const angle = Math.atan2(direction.x, direction.z);
+        this.node.setRotationFromEuler(0, angle * (180 / Math.PI), 0);
+
+        direction.normalize();
+        const movement = new Vec3(
+            direction.x * this.moveSpeed * deltaTime,
+            0,
+            direction.z * this.moveSpeed * deltaTime
+        );
+
+        this.node.worldPosition = new Vec3(
+            currentPosition.x + movement.x,
+            currentPosition.y,
+            currentPosition.z + movement.z
+        );
+
+        this.animator?.playAnimation(AnimationState.RUN);
     }
 
     private getWorldPositionFromTouch(event: EventTouch):Vec3 | null {
@@ -60,38 +104,6 @@ export class PlayerController extends Component {
 
     private _onTouchEnd() {
         this.isMoving = false;
-    }
-
-    update(deltaTime: number) {
-        if (!this.isMoving) return;
-
-        const currentPosition = this.node.worldPosition;
-        const direction = new Vec3();
-        Vec3.subtract(direction, this.targetPosition, currentPosition);
-        direction.y = 0;
-
-        const distance = direction.length();
-
-        if (distance < this.STOP_DISTANCE) {
-            this.isMoving = false;
-            return;
-        }
-
-        const angle = Math.atan2(direction.x, direction.z);
-        this.node.setRotationFromEuler(0, angle * (180 / Math.PI), 0);
-
-        direction.normalize();
-        const movement = new Vec3(
-            direction.x * this.moveSpeed * deltaTime,
-            0,
-            direction.z * this.moveSpeed * deltaTime
-        );
-
-        this.node.worldPosition = new Vec3(
-            currentPosition.x + movement.x,
-            currentPosition.y,
-            currentPosition.z + movement.z
-        );
     }
 }
 
