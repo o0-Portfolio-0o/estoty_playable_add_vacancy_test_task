1e18719f521fb5e7f1266249c4d360b49ecaa691
o0pmitev <pmitev.dev@gmail.com>
2026-03-28 07:26:53 +0200

    feat: add craft bench with upgrade modal HUD resource counter

---
+++ assets/scripts/objects/CraftBench.ts
@@ -0,0 +1,79 @@
+import { _decorator, Component, Node, Vec3 } from 'cc';
+import { GameManager, WeaponLevel } from '../managers/GameManager';
+import { ResourceManager } from '../managers/ResourceManager';
+import { PlayerController } from '../player/PlayerController';
+const { ccclass, property } = _decorator;
+
+@ccclass('CraftBench')
+export class CraftBench extends Component {
+
+    @property(Node)
+    public player: Node = null;
+
+    @property
+    public interactRange = 2.0;
+
+    private _playerController: PlayerController = null;
+    private _isPlayerInRange: boolean = false;
+    private _hasShownModal: boolean = false;
+
+
+    onLoad() {
+        if (this.player) {
+            this._playerController = this.player.getComponent(PlayerController);
+        }
+    }
+
+    public onUpgradeConfirmed(): void {
+        const weaponLevel = GameManager.instance?.weaponLevel;
+
+        if (weaponLevel === WeaponLevel.L1) {
+            ResourceManager.instance?.consumeForUpgrade(2);
+        } else if (weaponLevel === WeaponLevel.L2) {
+            ResourceManager.instance?.consumeForUpgrade(3);
+        }
+        GameManager.instance?.upgradeWeapon();
+        this._hasShownModal = false;
+    }
+
+    private _isInRange(): boolean {
+        if (!this.player) return;
+        const distance = Vec3.distance(
+            this.node.worldPosition,
+            this.player.worldPosition
+        );
+        return distance <= this.interactRange;
+    }
+
+    private _canUpgrade(): boolean {
+        const weaponLevel = GameManager.instance?.weaponLevel;
+        const rm = ResourceManager.instance;
+        if (!rm || !weaponLevel) return false;
+
+        if (weaponLevel === WeaponLevel.L1 && rm.canUpgradeToL2()) return true;
+        if (weaponLevel === WeaponLevel.L2 && rm.canUpgradeToL3()) return true;
+        return false;
+    }
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        const inRange = this._isInRange();
+        if (inRange && !this._isPlayerInRange) {
+            // Enter
+            this._isPlayerInRange = true;
+            if (this._canUpgrade() && !this._hasShownModal) {
+                this.node.emit('show-upgrade-modal');
+            }
+        }
+
+        if (!inRange && this._isPlayerInRange) {
+            // Left
+            this._isPlayerInRange = false;
+        }
+    }
+}
+
+
---
+++ assets/scripts/ui/HUD.ts
@@ -0,0 +1,78 @@
+import { _decorator, Component, Node, Label } from 'cc';
+import { ResourceManager } from '../managers/ResourceManager';
+import { GameManager, WeaponLevel } from '../managers/GameManager';
+import { CraftBench } from '../objects/CraftBench';
+const { ccclass, property } = _decorator;
+
+@ccclass('HUD')
+export class HUD extends Component {
+
+    @property(Label)
+    public resourceLabel: Label = null;
+ 
+    @property(Node)
+    public upgradeModal: Node = null;
+
+    @property(CraftBench)
+    public craftBench: CraftBench = null;
+
+    onLoad() {
+        //TODO: Extract to method attachlisteners
+        //TODO: replace strings for RESOURCE_CHANGE: 'resource-changed'
+
+        ResourceManager.instance?.node.on('resource-changed', this._updateResourceLabel, this);
+
+        this.craftBench?.node.on('show-upgrade-modal', this._showUpgradeModal, this);
+
+        if (this.upgradeModal) this.upgradeModal.active = false;
+        this._updateResourceLabel();
+    }
+
+    onDestroy(): void {
+        ResourceManager.instance?.node.off('resource-changed', this._updateResourceLabel, this);
+
+        this.craftBench?.node.off('show-upgrade-modal', this._showUpgradeModal, this);
+    }
+
+    public onUpgradeButtonClicked() {
+        this.craftBench?.onUpgradeConfirmed();
+        if (this.upgradeModal) this.upgradeModal.active = false;
+        this._updateResourceLabel();
+    }
+
+    public onModalClose() {
+        if (this.upgradeModal) this.upgradeModal.active = false;
+    }
+
+    private _updateResourceLabel() {
+        if (!this.resourceLabel) return;
+
+        const rm = ResourceManager.instance;
+
+        if (!rm) return;
+
+        const weaponLevel = GameManager.instance?.weaponLevel;
+
+        if (weaponLevel === WeaponLevel.L1) {
+            this.resourceLabel.string = `Wood: ${rm.resource1} / ${rm.resource1Required}`;
+        } else if (weaponLevel === WeaponLevel.L2) {
+            this.resourceLabel.string = `Scrap: ${rm.resource2} / ${rm.resource2Required}`;
+        } else {
+            this.resourceLabel.string = 'Weapon MAX';
+        }
+    }
+
+    private _showUpgradeModal() {
+        if  (this.upgradeModal) this.upgradeModal.active = true;
+    }
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
