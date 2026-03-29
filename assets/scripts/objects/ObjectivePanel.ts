c23505be2641bde2f93f09fe423d658ce1e5990a
o0pmitev <pmitev.dev@gmail.com>
2026-03-29 16:27:15 +0300

    feat: finish objective panel implement

--- assets/scripts/objects/ObjectivePanel.ts
+++ assets/scripts/objects/ObjectivePanel.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Sprite, Node, Color, tween, Vec3 } from 'cc';
+import { _decorator, Component, Sprite, Node, Color, tween, Tween, Vec3, sp } from 'cc';
 const { ccclass, property } = _decorator;
 import { GameManager, WeaponLevel } from '../managers/GameManager';
 import { ResourceManager } from '../managers/ResourceManager';
@@ -15,6 +15,9 @@ export class ObjectivePanel extends Component {
     @property(Sprite)
     public weaponIconL3: Sprite = null;
 
+    @property(Sprite)
+    public gateIcon: Sprite = null;
+
     private readonly FADED_ALPHA = 100;
     private readonly FULL_ALPHA = 255;
 
@@ -24,6 +27,7 @@ export class ObjectivePanel extends Component {
 
         this._setFaded(this.benchIcon);
         this._setFaded(this.weaponIconL2);
+        this._setFaded(this.gateIcon);
         this.weaponIconL3.node.active = false;
     }
 
@@ -54,12 +58,15 @@ export class ObjectivePanel extends Component {
             this._setFaded(this.benchIcon);
             this._setFaded(this.weaponIconL3);
         } else if (level === WeaponLevel.L3) {
-            this.node.active = false;
+            this.weaponIconL3.node.active = false;
+            this.benchIcon.node.active = false;
+            this._pulseToFull(this.gateIcon);
         }
     }
 
     private _setFaded(sprite: Sprite) {
         if (!sprite) return;
+        this._deactivateActiveAnim(sprite);
 
         sprite.color = new Color(255, 255, 255, this.FADED_ALPHA);
         sprite.node.setScale(1, 1, 1);
@@ -73,7 +80,27 @@ export class ObjectivePanel extends Component {
         tween(sprite.node)
             .to(0.15, {scale: new Vec3(1.3, 1.3, 1)})
             .to(0.15, {scale: new Vec3(1, 1, 1)})
+            .call(() => {
+                this._startActiveAnim(sprite);
+            })
             .start();
+    }
+
+    private _startActiveAnim(sprite: Sprite) {
+        if (!sprite) return;
+
+        tween(sprite.node)
+            .to(0.6, {scale: new Vec3(1.1, 1.1, 1.1)})
+            .to(0.6, {scale: new Vec3(1, 1, 1)})
+            .union()
+            .repeatForever()
+            .start();
+    }
+
+    private _deactivateActiveAnim(sprite: Sprite) {
+        if (!sprite) return;
+        Tween.stopAllByTarget(sprite.node);
+        sprite.node.setScale(1, 1, 1);
     }
 }
 
